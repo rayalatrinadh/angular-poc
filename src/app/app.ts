20b1f9ec5d9331357b25e64eff21816f4d308a19
TRINADH RAYALA <31477828+rayalatrinadh@users.noreply.github.com>
2025-12-14 14:39:49 -0600

    routes fix (#2)
    q
    w
    q
    
    clear
    exit
    q
    w
    :

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
 import { User } from "./user/user";
-import { ObservableDemo } from './observable-demo/observable-demo';
+// import { ObservableDemo } from './observable-demo/observable-demo';
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Header, User, ObservableDemo],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
